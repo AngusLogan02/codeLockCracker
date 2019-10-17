a6ee3e0281a0803fefd564f9257f9ae336653773
Angus Logan <41449334+AngusLogan02@users.noreply.github.com>
2019-10-17 20:16:29 +0100

    update

--- codeLockKillerv2.py
+++ codeLockKillerv2.py
@@ -1,16 +1,27 @@
 import keyboard as kb
 import time
+import msvcrt
+import sys
+
+def endProgram():
+    raise KeyboardInterrupt
+
+kb.add_hotkey("ctrl+space", endProgram)
 
 startingDigit = int(input("Thousand to start on (0 for default): "))
 print("Ok. Press CTRL + C when code has been broken.")
 
 time.sleep(3)
 
-while kb.is_pressed('/') == False:
+while True:
     for digit1 in range(startingDigit,10):
         for digit2 in range(0,10):
             for digit3 in range(0,10):
                 for digit4 in range(0,10):
+
+                    if kb.is_pressed('/'):
+                        sys.exit()
+                    
                     code = (str(digit1)+str(digit2)+str(digit3)+str(digit4))
                     print(code)
                     kb.write(code)
@@ -18,4 +29,3 @@ while kb.is_pressed('/') == False:
                     time.sleep(0.01)
                     for i in range(0,5):
                         kb.send('backspace')
-        
