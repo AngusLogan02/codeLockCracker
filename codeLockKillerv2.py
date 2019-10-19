7773f171e6c83b95a1f5b8dc9789d2829d093468
Angus Logan <41449334+AngusLogan02@users.noreply.github.com>
2019-10-19 23:27:12 +0000

    removed unnecessary while loop

--- codeLockKillerv2.py
+++ codeLockKillerv2.py
@@ -8,19 +8,20 @@ print("Ok. Press CTRL + C when code has been broken.")
 
 time.sleep(3)
 
-while True:
-    for digit1 in range(startingDigit,10):
-        for digit2 in range(0,10):
-            for digit3 in range(0,10):
-                for digit4 in range(0,10):
+for digit1 in range(startingDigit,10):
+    for digit2 in range(0,10):
+        for digit3 in range(0,10):
+            for digit4 in range(0,10):
 
-                    if kb.is_pressed(';'):
-                        sys.exit()
+                if kb.is_pressed(';'):
+                    sys.exit()
                     
-                    code = (str(digit1)+str(digit2)+str(digit3)+str(digit4))
-                    print(code)
-                    kb.write(code)
-                    kb.send('enter')
-                    time.sleep(0.01)
-                    for i in range(0,5):
-                        kb.send('backspace')
+                code = (str(digit1)+str(digit2)+str(digit3)+str(digit4))
+                print(code)
+                kb.write(code)
+                kb.send('enter')
+                time.sleep(0.01)
+                for i in range(0,5):
+                    kb.send('backspace')
+
+print("Mission failed; we'll get 'em next time.")
